51025b5374d05643aa2616c00762a639291cd114
Mateusz Kapała <matikap2@gmail.com>
2020-12-01 20:06:39 +0100

    Initial commit

---
+++ mqtt_example_publisher.py
@@ -0,0 +1,66 @@
+import paho.mqtt.client as mqtt
+import kpn_senml as senml
+import time
+import random
+
+# Constants
+DEVICE_SN_PUBLISHER = "0xDEADBEEF"
+BASE_NAME_PUBLISHER = "urn:dev:" + DEVICE_SN_PUBLISHER + ":sensors:"
+BROKER_HOST = "rpi.mateuszkapala.eu"
+BROKER_PORT = 1883
+MQTT_TOPIC = "/example/living_room/sensors"
+SLEEP_TIME = 10
+
+
+# Data section
+def get_current_temperature():
+    return random.uniform(25.0, 35.0)
+
+
+def get_current_humidity():
+    return random.uniform(0.0, 100.0)
+
+
+def build_sensor_senml_object(temperature: float, humidity: float):
+    pack = senml.SenmlPack(BASE_NAME_PUBLISHER)                  # set base name
+    pack.base_time = time.time()                                 # set base time
+    temp = senml.SenmlRecord("temperature",
+                             unit=senml.SenmlUnits.SENML_UNIT_DEGREES_CELSIUS,
+                             value=temperature)             # add room temperature record
+    humidity = senml.SenmlRecord("humidity",
+                                 unit=senml.SenmlUnits.SENML_UNIT_RELATIVE_HUMIDITY,
+                                 value=humidity)            # add room relative humidity record
+    pack.add(temp)
+    pack.add(humidity)
+    return pack.to_json()
+
+
+# MQTT section
+def on_publish(mqttc, obj, mid):
+    print("=====> Published! Message ID: " + str(mid))
+
+
+# Main script execution
+if __name__ == "__main__":
+    # Setup MQTT Paho client
+    client = mqtt.Client(DEVICE_SN_PUBLISHER)
+    client.on_publish = on_publish
+
+    while True:
+        # Connect
+        client.connect(BROKER_HOST, BROKER_PORT)
+
+        # Prepare sensor data and SenML object
+        print("============================================================")
+        msg = build_sensor_senml_object(get_current_temperature(),
+                                        get_current_humidity())
+        print(f"Data to be published: {msg}")
+
+        # Publish to MQTT_TOPIC
+        client.publish(MQTT_TOPIC, msg, qos=0)
+
+        # Disconnect
+        client.disconnect()
+
+        # Wait for next iteration
+        time.sleep(SLEEP_TIME)
---
+++ mqtt_example_subscriber.py
@@ -0,0 +1,81 @@
+import paho.mqtt.client as mqtt
+import kpn_senml as senml
+import time
+import random
+
+# Constants
+DEVICE_SN_RECEIVER = "0xBAADF00D"
+DEVICE_SN_PUBLISHER = "0xDEADBEEF"
+BASE_NAME_PUBLISHER = "urn:dev:" + DEVICE_SN_PUBLISHER + ":sensors:"
+BROKER_HOST = "rpi.mateuszkapala.eu"
+BROKER_PORT = 1883
+MQTT_TOPIC = "/example/living_room/sensors"
+TEMP_LOW = 27.0
+TEMP_HIGH = 30.0
+
+
+# Utilities
+def static_vars(**kwargs):
+    def decorate(func):
+        for k in kwargs:
+            setattr(func, k, kwargs[k])
+        return func
+    return decorate
+
+
+# Data section
+@static_vars(heater_on=False)
+def change_heater_state(current_temperature: float):
+    if change_heater_state.heater_on and current_temperature > TEMP_HIGH:
+        change_heater_state.heater_on = False
+        print("=====> Turning off!")
+    elif not change_heater_state.heater_on and current_temperature < TEMP_LOW:
+        change_heater_state.heater_on = True
+        print("=====> Turning on!")
+    else:
+        print(f"=====> Current heater status: {change_heater_state.heater_on}")
+
+
+def handle_temp_value(record):
+    print(f"Current temperature value: {record.value}")
+    change_heater_state(record.value)
+
+
+# MQTT section
+def on_message(mqttc, obj, msg):
+    print("============================================================")
+    recv_msg = msg.payload.decode('ASCII')
+    print(f"Topic: {msg.topic}")
+    print(f"Recived message: {recv_msg}")
+
+    # Setup SenML decoder
+    pack = senml.SenmlPack(BASE_NAME_PUBLISHER)
+    temp = senml.SenmlRecord("temperature", callback=handle_temp_value)
+    pack.add(temp)
+
+    # Parse
+    try:
+        pack.from_json(recv_msg)
+    except:
+        print("Payload is not usable!")
+
+
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    print(f"Subscribed! ID: {mid}")
+
+
+# Main script execution
+if __name__ == "__main__":
+    # Setup MQTT Paho client
+    client = mqtt.Client(DEVICE_SN_RECEIVER)
+    client.on_message = on_message
+    client.on_subscribe = on_subscribe
+
+    # Connect
+    client.connect(BROKER_HOST, BROKER_PORT, 60)
+
+    # Subscribe
+    client.subscribe(MQTT_TOPIC, 0)
+
+    # Wait for messages
+    client.loop_forever()
