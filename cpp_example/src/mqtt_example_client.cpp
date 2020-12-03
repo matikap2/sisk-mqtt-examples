30a508c6863aa8174ada87bf978b953213f3d7e3
Mateusz Kapała <matikap2@gmail.com>
2020-12-03 21:39:43 +0100

    Simple MQTT Async pub-sub client example

--- cpp_example/src/mqtt_example_client.cpp
+++ cpp_example/src/mqtt_example_client.cpp
@@ -1,74 +1,3 @@
-// #include <iostream>
-
-// int main()
-// {
-//     std::cout << "Test CMake" << std::endl;
-
-//     return 0;
-// }
-
-// #include <iostream>
-// #include <cstdlib>
-// #include <string>
-// #include <thread>	// For sleep
-// #include <atomic>
-// #include <chrono>
-// #include <cstring>
-// #include "MQTTClient.h"
-
-// #define ADDRESS     "tcp://rpi.mateuszkapala.eu:1883"
-// #define CLIENTID    "ExampleClientPub"
-// #define TOPIC       "/test"
-// #define PAYLOAD     "Hello from the client C side!"
-// #define QOS         0
-// #define TIMEOUT     10000L
-
-// int main(int argc, char* argv[])
-// {
-//     MQTTClient client;
-//     MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-//     MQTTClient_message pubmsg = MQTTClient_message_initializer;
-//     MQTTClient_deliveryToken token;
-//     int rc;
-
-//     if ((rc = MQTTClient_create(&client, ADDRESS, CLIENTID,
-//         MQTTCLIENT_PERSISTENCE_NONE, NULL)) != MQTTCLIENT_SUCCESS)
-//     {
-//          printf("Failed to create client, return code %d\n", rc);
-//          exit(EXIT_FAILURE);
-//     }
-
-//     conn_opts.keepAliveInterval = 20;
-//     conn_opts.cleansession = 1;
-//     if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
-//     {
-//         printf("Failed to connect, return code %d\n", rc);
-//         exit(EXIT_FAILURE);
-//     }
-
-//     char* payload = PAYLOAD;
-//     pubmsg.payload = payload;
-//     pubmsg.payloadlen = (int)strlen(PAYLOAD);
-//     pubmsg.qos = QOS;
-//     pubmsg.retained = 0;
-//     if ((rc = MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token)) != MQTTCLIENT_SUCCESS)
-//     {
-//          printf("Failed to publish message, return code %d\n", rc);
-//          exit(EXIT_FAILURE);
-//     }
-
-//     printf("Waiting for up to %d seconds for publication of %s\n"
-//             "on topic %s for client with ClientID: %s\n",
-//             (int)(TIMEOUT/1000), PAYLOAD, TOPIC, CLIENTID);
-//     rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
-//     printf("Message with delivery token %d delivered\n", token);
-
-//     if ((rc = MQTTClient_disconnect(client, 10000)) != MQTTCLIENT_SUCCESS)
-//     	printf("Failed to disconnect, return code %d\n", rc);
-//     MQTTClient_destroy(&client);
-//     return rc;
-// }
-
 #include <random>
 #include <string>
 #include <thread>
@@ -79,84 +8,111 @@
 #include <ctime>
 #include "mqtt/async_client.h"
 
-using namespace std;
-using namespace std::chrono;
+//-----------------------------------------------------------------------------
 
-const std::string DFLT_ADDRESS { "tcp://rpi.mateuszkapala.eu:1883" };
+/* Configuration. */
+const std::string BROKER = "tcp://rpi.mateuszkapala.eu:1883";
+const std::string CLIENT_ID = "cpp_async_mqtt_client";
+const std::string TOPIC = "example/cpp/random";
+const int QOS = 1;
+const bool RETAINED = false;
+const auto TIMEOUT = std::chrono::seconds(10);
+const int MAX_ACTIONS = 4;
 
-const string TOPIC { "data/rand" };
-const int	 QOS = 1;
+/* Application flags. */
+bool finished_publish = false;
+bool finished_subscribe = false;
 
-const auto PERIOD = seconds(5);
+//-----------------------------------------------------------------------------
 
-const int MAX_BUFFERED_MSGS = 120;	// 120 * 5sec => 10min off-line buffering
+/* A callback class for use with the main MQTT client. */
+class callback : public virtual mqtt::callback
+{
+public:
+	/* Method called after successfully published messege. */
+	void delivery_complete(mqtt::delivery_token_ptr tok) override 
+	{
+		static int cnt = 0;
+		std::cout << "\t#" << ++cnt << " -> Delivery complete for token: "
+			<< (tok ? tok->get_message_id() : -1) << std::endl;
 
-const string PERSIST_DIR { "data-persist" };
+		if (cnt >= MAX_ACTIONS)
+			finished_publish = true;
+	}
 
-/////////////////////////////////////////////////////////////////////////////
+	/* Method called after message arrives at currently subscribed topic. */
+	void message_arrived(mqtt::const_message_ptr msg) override 
+	{
+		static int cnt = 0;
+		std::cout << "\t#" << ++cnt << " ->> Message arrived! " <<  "topic: '" << msg->get_topic() << "' " << 
+		"payload: '" << msg->to_string() << "'\n" << std::endl;
+
+		if (cnt >= MAX_ACTIONS)
+			finished_subscribe = true;
+	}
+
+};
+
+//-----------------------------------------------------------------------------
 
 int main(int argc, char* argv[])
 {
-    try {
-	string address = (argc > 1) ? string(argv[1]) : DFLT_ADDRESS;
-
-	mqtt::async_client cli(address, "", MAX_BUFFERED_MSGS, PERSIST_DIR);
-
+	/* Create asynchronous client object that use non-blocking methods and
+		allow us to run an operations in background. */
+	mqtt::async_client client(BROKER, CLIENT_ID);
 	mqtt::connect_options connOpts;
-	connOpts.set_keep_alive_interval(MAX_BUFFERED_MSGS * PERIOD);
 	connOpts.set_clean_session(true);
 	connOpts.set_automatic_reconnect(true);
 
-	// Create a topic object. This is a conventience since we will
-	// repeatedly publish messages with the same parameters.
-	mqtt::topic top(cli, TOPIC, QOS, true);
+	callback cb;
+	client.set_callback(cb);
 
-	// Random number generator [0 - 100]
-	random_device rnd;
-    mt19937 gen(rnd());
-    uniform_int_distribution<> dis(0, 100);
+	/* Create a topic object. This is a convenience since we will
+		repeatedly publish messages with the same parameters. */
+	mqtt::topic topic(client, TOPIC, QOS, RETAINED);
 
-	
-		// Connect to the MQTT broker
-		cout << "Connecting to server '" << address << "'..." << flush;
-		cli.connect(connOpts)->wait();
-		cout << "OK\n" << endl;
+	/* Setup of RNG in range [0 - 100] */
+	std::random_device rnd;							// TRNG
+    std::mt19937 gen(rnd());						// Mersenne Twister 19937 PRNG
+    std::uniform_int_distribution<> dis(0, 100);	// Specify numbers range
 
-		char tmbuf[32];
-		unsigned nsample = 0;
+    try 
+	{
+		/* Connect to the MQTT broker. */
+		std::cout << "Connecting to server '" << BROKER << "'... ";
+		client.connect(connOpts)->wait();
+		std::cout << "OK\n" << std::endl;
 
-		// The time at which to reads the next sample, starting now
-		auto tm = steady_clock::now();
+		/* Subscribe to topic on which we will be publishing messages. */
+		client.subscribe(TOPIC, QOS);
 
-		while (true) {
-			// Pace the samples to the desired rate
-			this_thread::sleep_until(tm);
+		/* Main application loop. */
+		while (!(finished_publish && finished_subscribe))
+		{
+			std::this_thread::sleep_for(std::chrono::seconds(2));
 
-			// Get a timestamp and format as a string
-			time_t t = system_clock::to_time_t(system_clock::now());
-			strftime(tmbuf, sizeof(tmbuf), "%F %T", localtime(&t));
+			if (!finished_publish)
+			{
+				/* Generate some data. */
+				int x = dis(gen);
 
-			// Simulate reading some data
-			int x = dis(gen);
+				/* Create some simple payload, ex. JSON object. */
+				std::string payload = "{\"random\": \"" + std::to_string(x) + "\"}";
+				std::cout << payload << std::endl;
 
-			// Create the payload as a text CSV string
-			string payload = to_string(++nsample) + "," +
-								tmbuf + "," + to_string(x);
-			cout << payload << endl;
-
-			// Publish to the topic
-			top.publish(std::move(payload));
-
-			tm += PERIOD;
+				/* Publish to the topic. */
+				topic.publish(payload);
+			}
 		}
 
-		// Disconnect
-		cout << "\nDisconnecting..." << flush;
-		cli.disconnect()->wait();
-		cout << "OK" << endl;
-	}
-	catch (const mqtt::exception& exc) {
-		cerr << exc.what() << endl;
+		/* Disconnect from broker */
+		std::cout << "\nDisconnecting... ";
+		client.disconnect()->wait();
+		std::cout << "OK" << std::endl;
+	} 
+	catch (const mqtt::exception& exc) 
+	{
+		std::cerr << exc.what() << std::endl;
 		return 1;
 	}
 
